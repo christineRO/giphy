9d7f4c8c67b03fb0035bff77f2fedb3a8feee6c9
christineRO <christinerole@icloud.com>
2020-03-10 01:25:04 +0000

    cool

--- js/script.js
+++ js/script.js
@@ -4,8 +4,25 @@
 /* global $ */
 
 $("#search-button").click(function(){
-  
-  
-  
-});
+    
+    let userInput = $("#search-term").val();
+    console.log(userInput);
 
+    let apiURL = `https://api.giphy.com/v1/gifs/search?q=${userInput}&rating=pg&api_key=dc6zaTOxFJmzC`
+    console.log(apiURL);
+
+        fetch(apiURL)
+        .then(function(response) {
+          return response.json();
+    })
+    .then(function(data) {
+
+    let gifURL = data.data[0].images.original.url;
+            
+    $(".gifResults").append(`<img src="${gifURL}">`);
+    
+    console.log(gifURL);
+
+    console.log("hi");
+});
+});
